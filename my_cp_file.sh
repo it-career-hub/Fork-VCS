f6992a90063f35e5cba586d306368c0701e8898a
Juliya <jursylian@gmail.com>
2025-07-15 12:52:10 +0300

    Added one more check

--- my_cp_file.sh
+++ my_cp_file.sh
@@ -6,6 +6,10 @@ if [[ -z "$2" ]]; then
   else
     filename=$(basename "$1")
     dirname=$(dirname "$2")
+    if [[ ! -z "$dirname" ]]; then
+  echo "Directory "$dirname" doesn't exist"
+  exit 1
+  else
     mkdir -p "$dirname"
     echo "Copying $filename to $dirname"
        cp "$1" "$2"
