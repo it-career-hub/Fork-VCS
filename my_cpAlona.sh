cbe8fd311174b18b9705fb5cc5b07764a3741d5f
AlonaHrytsenko <alenagrytsenko111@gmail.com>
2025-07-15 12:04:24 +0200

    modified2 my_cpAlona.sh

--- my_cpAlona.sh
+++ my_cpAlona.sh
@@ -7,14 +7,11 @@ else
  filename=$(basename "$1")
  dirname=$(dirname "$2")
 
-if [[ ! -z "$dirname" ]]; then
-
-mkdir -p "$dirname"
-echo "Copying $filename to $dirname"
+  if [[ ! -z "$dirname" ]]; then
+    mkdir -p "$dirname"
+  fi
+ echo "Copying $filename to $dirname"
  cp "$1" "$2"
  echo "File copied Successful"
  exit 1
-
- fi
-
 fi
