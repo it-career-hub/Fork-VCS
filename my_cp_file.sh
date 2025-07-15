231a466699530cbaa60f3d99b1c7eb0c2b1b6ffd
Juliya <91060705+jursylian@users.noreply.github.com>
2025-07-15 12:32:02 +0300

    Add file my_cp_file.sh from jursylian.
    * Add file my_cp_file.sh
    
    * Add "Done"

---
+++ my_cp_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+if [[ -z "$2" ]]; then
+  echo "Bad command line arguments. "
+  exit 1
+  else
+    filename=$(basename "$1")
+    dirname=$(dirname "$2")
+    mkdir -p "$dirname"
+    echo "Copying $filename to $dirname"
+       cp "$1" "$2"
+       echo "File copied successfully."
+       echo "Done"
+  fi
