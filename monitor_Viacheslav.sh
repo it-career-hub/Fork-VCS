8696b1eeba251a34e30695f53ff712bd53777cc1
Viacheslav_Andreev <slava.avjv@gmail.com>
2026-05-19 12:04:23 +0300

    Edit monitor_Viacheslav.sh

--- monitor_Viacheslav.sh
+++ monitor_Viacheslav.sh
@@ -13,5 +13,5 @@ echo "Top Processes:"
 echo "$top_processes"
 process_count=$(ps -ef | grep -v "PID" | wc -l)
 echo "Total Processes: $process_count"
-user_count=$(who | wc -l)
+user_count=$(w | tail -n +3 |wc -l)
 echo "Logged in Users: $user_count"
